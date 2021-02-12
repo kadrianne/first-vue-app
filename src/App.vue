90c2c0469aedce228b3a80f5e5455227c2a91514
Kristine Du <kristine.a.du@gmail.com>
2021-02-12 09:41:33 -0700

    Build basic app.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 :class="this.titleColor">{{ this.title }}</h1>
+    <button @click="handleClick">Toggle Title Color</button>
+    <CardsContainer @delete-pokemon="deletePokemon" :pokemon="this.pokemon" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CardsContainer from "./components/CardsContainer";
 
 export default {
   name: "App",
+  mounted: function() {
+    this.$nextTick(function() {
+      fetch("https://pokeapi.co/api/v2/pokemon/?limit=150")
+        .then(response => response.json())
+        .then(data => {
+          const allPokemon = data.results.map((pokemon, index) => {
+            const id = index + 1;
+            return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+          });
+          Promise.all(allPokemon)
+            .then(response =>
+              Promise.all(response.map(pokeResult => pokeResult.json()))
+            )
+            .then(pokemon => (this.pokemon = pokemon));
+        });
+    });
+  },
+  data() {
+    return {
+      title: "Pokemon App",
+      titleColor: "red",
+      pokemon: []
+    };
+  },
   components: {
-    HelloWorld
+    CardsContainer
+  },
+  computed: {
+    titleLength() {
+      return this.title.length;
+    }
+  },
+  methods: {
+    handleClick() {
+      this.titleColor == "red"
+        ? (this.titleColor = "blue")
+        : (this.titleColor = "red");
+    },
+    deletePokemon(event, id) {
+      console.log(id, event);
+    }
   }
 };
 </script>
 
 <style>
+.red {
+  color: "red";
+}
+
+.blue {
+  color: "blue";
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,37 @@
+<template>
+  <section id="cards-container" class="card-container">
+    <h2>Cards Container</h2>
+    <ul id="pokemon">
+      <div class="card" v-for="onePokemon in pokemon" :key="onePokemon.id">
+        <h3>{{ onePokemon.name }}</h3>
+        <img :src="onePokemon.sprites.front_default" />
+        <button @click="this.$emit(deletePokemon)">X</button>
+      </div>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "CardsContainer",
+  props: {
+    pokemon: Array,
+    deletePokemon: Function
+  }
+};
+</script>
+
+<style scoped>
+.card-container {
+  border: 1px solid black;
+}
+
+#pokemon {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card {
+  border: 1px solid black;
+}
+</style>
